199a90e942ee1d0067c9802273584e2ef34e4bed
Dillon Hailey <181585418+Velarken@users.noreply.github.com>
2026-02-11 13:11:48 -0500

    created router for all post routes

---
+++ src/routes/postRouter.js
@@ -0,0 +1,33 @@
+const { Router } = require('express');
+const { post } = require('../../lib/prisma');
+
+const postRouter = Router();
+
+postRouter.get('/', ( req, res ) => {
+    res.json({
+        message: 'Show all posts.'
+    });
+});
+postRouter.get('/search', ( req, res ) => {
+    res.json({
+        message: 'Show search form with the following fields',
+        fields: {
+            user: 'Search for all posts by a given user.',
+            keyword: 'Search for all posts with a given keyword.',
+            category: 'Search for all posts within a given category.'
+        }
+    })
+})
+postRouter.get('/search/:userId', ( req, res ) => {
+    // logic to find posts that were created by given userId
+    res.json({
+        message: 'Showing all posts for provided userId.'
+    });
+});
+postRouter.get('/:postId/comments', ( req, res ) => {
+    res.json({
+        message: 'Showing all comments on a single post with provided postId'
+    })
+})
+
+module.exports = postRouter;
